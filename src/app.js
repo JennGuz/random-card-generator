10735b53bb08b31ad51a3d9660d840ed52efbf9b
JennGuz <143773875+JennGuz@users.noreply.github.com>
2024-04-16 01:47:50 +0000

    finish project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+};
+let iconContainer = document.querySelector(".symbol");
+let iconContainer2 = document.querySelector(".symbol2");
+let icons = ["♥", "♦", "♠", "♣"];
+
+let numberContainer = document.querySelector(".number");
+
+window.changeCard = function changeCard() {
+  let iconAleatorio = Math.floor(Math.random() * icons.length);
+  let numberAleatorio = Math.floor(Math.random() * 13);
+
+  iconContainer.innerHTML = icons[iconAleatorio];
+  iconContainer2.innerHTML = icons[iconAleatorio];
+  numberContainer.innerHTML = numberAleatorio;
+
+  if (icons[iconAleatorio] === "♥") {
+    iconContainer.style.color = "red";
+    iconContainer2.style.color = "red";
+    numberContainer.style.color = "red";
+  } else if (
+    icons[iconAleatorio] === "♦" ||
+    icons[iconAleatorio] === "♠" ||
+    icons[iconAleatorio] === "♣"
+  ) {
+    iconContainer.style.color = "black";
+    iconContainer2.style.color = "black";
+    numberContainer.style.color = "black";
+  }
+
+  iconContainer2.classList.add("reverse");
 };
